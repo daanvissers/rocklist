bf007e2ecb0b6984c930917d40a8b63c1ef513b3
Daan Vissers <daanvissers81@gmail.com>
2020-12-17 14:10:48 +0100

    Add routes, artist detail page

--- src/app/artists/pages/details/details.component.ts
+++ src/app/artists/pages/details/details.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import { Observable, Subscription } from 'rxjs';
+import { Artist } from '../../artist';
+import { ArtistService } from '../../artist.service';
 
 @Component({
   selector: 'app-details',
@@ -7,9 +11,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailsComponent implements OnInit {
 
-  constructor() { }
+  artist: Observable<Artist>;
+  id: number;
+
+  constructor(private artistService: ArtistService, private route: ActivatedRoute) {
+    this.artist = new Observable<Artist>();
+    this.id = +this.route.snapshot.paramMap.get('id')!;
+  }
 
   ngOnInit(): void {
+    // Get param from snapshot, as this component does not intend to update URL
+    this.artist = this.artistService.get(this.id);
   }
 
 }
